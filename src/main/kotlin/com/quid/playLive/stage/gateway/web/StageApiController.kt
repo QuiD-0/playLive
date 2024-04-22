4a0e3b7eef32ea79f60cb32ffebd183cfd51bd2d
QuiD-0 <wodnd101@naver.com>
2024-04-22 12:47:59 +0900

    방송 시작, 종료 훅 생성

--- src/main/kotlin/com/quid/playLive/stage/gateway/web/StageApiController.kt
+++ src/main/kotlin/com/quid/playLive/stage/gateway/web/StageApiController.kt
@@ -2,10 +2,7 @@ package com.quid.playLive.stage.gateway.web
 
 import com.quid.playLive.config.ResourcePath
 import com.quid.playLive.stage.usecase.ResourceConnector
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/stage")
@@ -18,4 +15,31 @@ class StageApiController(
 
     @GetMapping("/radio/{channel}")
     fun getRadio(@PathVariable channel: String) = liveStream(channel, path.radio)
+
+    @PostMapping("/play", consumes = ["application/x-www-form-urlencoded"])
+    fun play(request: NginxNotifyRequest) {
+        println("play")
+        println(request)
+        //방송 시작을 알수있다.
+        // uptime기록 + 알림 보내는 서비스 등등 추가
+    }
+
+    @PostMapping("/play_done", consumes = ["application/x-www-form-urlencoded"])
+    fun playDone(request: NginxNotifyRequest) {
+        println("play_done")
+        println(request)
+        //방송 종료를 알수있다.
+    }
+
+    data class NginxNotifyRequest(
+        val call: String?,
+        val addr: String?,
+        val clientid: String?,
+        val app: String?,
+        val flashVer: String?,
+        val swfUrl: String?,
+        val tcUrl: String?,
+        val pageUrl: String?,
+        val name: String?
+    )
 }
