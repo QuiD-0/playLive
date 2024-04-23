68e60f4b8d6247debe40d4b6589633f6b5e8c640
QuiD-0 <wodnd101@naver.com>
2024-04-23 17:05:14 +0900

    vue 적용

--- src/main/kotlin/com/quid/playLive/global/web/WebController.kt
+++ src/main/kotlin/com/quid/playLive/global/web/WebController.kt
@@ -1,7 +1,6 @@
 package com.quid.playLive.global.web
 
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
@@ -9,8 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable
 class WebController {
 
     @GetMapping("/live/{channel}")
-    fun getChannel(@PathVariable channel: String, model: Model): String {
-        model.addAttribute("channel", channel)
+    fun getChannel(@PathVariable channel: String): String {
         return "live"
     }
 }
