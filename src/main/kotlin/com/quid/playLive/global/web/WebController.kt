4d14e08d324f2a583eaa0d4b7f99bfad81997fc0
QuiD-0 <wodnd101@naver.com>
2024-05-16 13:02:59 +0900

    존재하지 않는 사용자 페이지

--- src/main/kotlin/com/quid/playLive/global/web/WebController.kt
+++ src/main/kotlin/com/quid/playLive/global/web/WebController.kt
@@ -1,11 +1,14 @@
 package com.quid.playLive.global.web
 
+import com.quid.playLive.member.usecase.FindMember
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
-class WebController {
+class WebController(
+    private val member: FindMember
+) {
 
     @GetMapping("/")
     fun index(): String {
@@ -13,7 +16,10 @@ class WebController {
     }
 
     @GetMapping("/live/{channel}")
-    fun getChannel(@PathVariable channel: String): String {
-        return "live"
-    }
+    fun getChannel(@PathVariable channel: String): String =
+        if (member.exists(channel)) {
+            "live"
+        } else {
+            "channel-not-found"
+        }
 }
