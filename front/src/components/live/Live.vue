c6090d9c1fac208b28c16832ec9daafad168d417
quid-0 <wodnd101@naver.com>
2024-12-13 20:04:06 +0900

    채널상태에 따른 분기처리

--- front/src/components/live/Live.vue
+++ front/src/components/live/Live.vue
@@ -1,9 +1,17 @@
 <script setup>
 import VideoBox from "@/components/live/VideoBox.vue";
 import ViewCounter from "@/components/live/ViewCounter.vue";
+import NotFound from "@/components/live/NotFound.vue";
+
+const isAvailable = true;
 </script>
 
 <template>
-    <VideoBox />
-    <ViewCounter />
+  <div v-if="isAvailable">
+    <VideoBox/>
+    <ViewCounter/>
+  </div>
+  <div v-else>
+    <NotFound/>
+  </div>
 </template>
---
+++ front/src/components/live/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>Channel not found...</div>
+</template>
+
+<style scoped>
+
+</style>
