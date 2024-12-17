7fc55b23e8c058b7e3bd15f9c0e48b804e2bf1a4
quid-0 <wodnd101@naver.com>
2024-12-17 22:55:53 +0900

    라이브카드 컴포넌트 분리

---
+++ front/src/components/live/LiveCard.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Thumbnail from "@/components/live/Thumbnail.vue";
+
+defineProps(['live']);
+</script>
+
+<template>
+  <div class="live__card" @click="$router.push(`/live/${live.username}`)">
+    <Thumbnail :channel="live.username"/>
+    <div class="title">{{ live.title }}</div>
+    <div class="nickname">{{ live.nickname }}</div>
+  </div>
+</template>
+
+<style scoped>
+
+.live__card {
+  display: inline-block;
+  width: 300px;
+  height: 230px;
+  margin: 10px;
+}
+
+.title {
+  height: 23px;
+  font-size: 1.2rem;
+}
+
+.nickname {
+  font-size: 1rem;
+  color: gray;
+}
+</style>
--- front/src/components/live/LiveList.vue
+++ front/src/components/live/LiveList.vue
@@ -1,7 +1,7 @@
 <script setup>
 import {onMounted, ref} from "vue";
 import instance from "@/module/axiosFactory.js";
-import Thumbnail from "@/components/live/Thumbnail.vue";
+import LiveCard from "@/components/live/LiveCard.vue";
 
 const lives = ref([]);
 
@@ -17,19 +17,9 @@ const fetchLives = async () => {
 
 <template>
   <div class="main__container">
-    <div class="heading">
-      Now Live!
-    </div>
+    <div class="heading">Now Live!</div>
     <div class="live__box">
-      <div
-          v-for="live in lives"
-          :key="live.id"
-          class="live__card"
-          @click="$router.push(`/live/${live.username}`)">
-        <Thumbnail :channel="live.username"/>
-        <div class="title">{{ live.title }}</div>
-        <div class="nickname">{{ live.nickname }}</div>
-      </div>
+      <LiveCard v-for="live in lives" :key="live.id" :live="live"/>
     </div>
   </div>
 </template>
@@ -53,22 +43,5 @@ const fetchLives = async () => {
   display: flex;
   flex-wrap: wrap;
   justify-content: left;
-}
-
-.live__card {
-  display: inline-block;
-  width: 300px;
-  height: 230px;
-  margin: 10px;
-}
-
-.title {
-  height: 23px;
-  font-size: 1.2rem;
-}
-
-.nickname {
-  font-size: 1rem;
-  color: gray;
 }
 </style>
