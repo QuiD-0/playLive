d6cef35c6dca97ad59a1c5d38649e3089e5dce20
quid-0 <wodnd101@naver.com>
2024-12-17 23:17:24 +0900

    라이브 마크 적용

--- front/src/components/live/LiveCard.vue
+++ front/src/components/live/LiveCard.vue
@@ -6,19 +6,39 @@ defineProps(['live']);
 
 <template>
   <div class="live__card" @click="$router.push(`/live/${live.username}`)">
-    <Thumbnail :channel="live.username"/>
+    <div class="thumbnail__wrapper">
+      <div class="live__mark">LIVE</div>
+      <Thumbnail :channel="live.username" />
+    </div>
     <div class="title">{{ live.title }}</div>
     <div class="nickname">{{ live.nickname }}</div>
   </div>
 </template>
 
 <style scoped>
-
 .live__card {
   display: inline-block;
   width: 300px;
   height: 230px;
-  margin: 10px;
+  margin: 10px 20px 10px 0;
+}
+
+.thumbnail__wrapper {
+  position: relative;
+  width: 100%;
+  height: 180px;
+}
+
+.live__mark {
+  position: absolute;
+  top: 8px;
+  left: 8px;
+  padding: 3px 8px;
+  background-color: red;
+  color: white;
+  font-size: 1rem;
+  border-radius: 5px;
+  z-index: 10;
 }
 
 .title {
