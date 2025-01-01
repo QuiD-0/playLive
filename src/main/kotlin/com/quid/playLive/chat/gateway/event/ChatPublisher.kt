c78f635b4c38420d8e1cab73a188c195d1d07c65
quid-0 <wodnd101@naver.com>
2025-01-01 22:22:42 +0900

    redis publish

--- src/main/kotlin/com/quid/playLive/chat/gateway/event/ChatPublisher.kt
+++ src/main/kotlin/com/quid/playLive/chat/gateway/event/ChatPublisher.kt
@@ -1,11 +1,14 @@
 package com.quid.playLive.chat.gateway.event
 
 import com.quid.playLive.chat.domain.Chat
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Component
 
 @Component
-class ChatPublisher {
+class ChatPublisher(
+    private val redisTemplate: RedisTemplate<String, String>,
+) {
     fun publish(chat: Chat) {
-        TODO("Not yet implemented")
+        redisTemplate.convertAndSend("chat", chat.toJson())
     }
 }
