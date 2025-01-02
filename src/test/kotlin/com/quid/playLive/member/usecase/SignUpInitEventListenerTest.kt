fdd0737babeeae8e427a371135519ad773f4515f
quid-0 <wodnd101@naver.com>
2025-01-02 14:30:14 +0900

    테스트 수정

--- src/test/kotlin/com/quid/playLive/member/usecase/SignUpInitEventListenerTest.kt
+++ src/test/kotlin/com/quid/playLive/member/usecase/SignUpInitEventListenerTest.kt
@@ -2,15 +2,11 @@ package com.quid.playLive.member.usecase
 
 import com.quid.playLive.fixture.MemberFixture
 import com.quid.playLive.stage.gateway.repository.StageInfoRepository
-import org.junit.jupiter.api.Assertions.assertDoesNotThrow
 import org.junit.jupiter.api.Assertions.assertNotNull
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.annotation.Rollback
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.transaction.AfterTransaction
-import org.springframework.transaction.annotation.Transactional
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -22,13 +18,6 @@ class SignUpInitEventListenerTest(
 ) {
 
     private val member = MemberFixture.member
-
-    @Test
-    @Transactional
-    @Rollback(false)
-    fun signUpSuccess() {
-        assertDoesNotThrow { signUp(member) }
-    }
 
     @AfterTransaction
     fun afterTransaction() {
